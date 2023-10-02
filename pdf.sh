2b9f1d79e132ee3731f6b219eb9436ceb206140c
Gwannon <monclus.jorge@gmail.com>
2023-10-02 13:36:28 +0200

    Mejora indice

--- pdf.sh
+++ pdf.sh
@@ -2,7 +2,7 @@
 
 chromium --no-sandbox --headless --disable-gpu --no-pdf-header-footer --disable-back-forward-cache-for-cache-control-no-store-page --print-to-pdf=./temp.pdf ./index.html
 
-exiftool -overwrite_original -Title="¡Arrr! Cthulhu - Aventuras de piratas en el Caribe de los mitos de Cthulhu. Versión 0.1" -Author="@Gwannon" -Subject="¡Arrr! Cthulhu es una ambientación para SWADE con el que podrás jugar aventuras de piratas en el Caribe de los Mitos de Cthulhu. Versión 1.0.1" ./temp.pdf
+exiftool -overwrite_original -Title="Mil y Un Ghūles - Ambientación de Savage Worlds para poder jugar relatos de horror cósmico en las Mil y Una Noches. Versión 0.1" -Author="@Gwannon" -Subject="Mil y Un Ghūles es una ambientación de Savage Worlds Edición Aventura para poder jugar relatos de horror cósmico en las Mil y Una Noches. Versión 0.1" ./temp.pdf
 
 php pdfIndexGenerator.php > pdf.info
 
