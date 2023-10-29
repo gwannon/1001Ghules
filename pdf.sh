c08bf24de8241a2ba647626050f12888b1161c80
Gwannon <monclus.jorge@gmail.com>
2023-10-29 13:13:51 +0100

    Grimorio

--- pdf.sh
+++ pdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chromium --no-sandbox --headless --disable-gpu --no-pdf-header-footer --disable-back-forward-cache-for-cache-control-no-store-page --print-to-pdf=./temp.pdf https://1001ghules.gwannon.com/index.html
+chromium --no-sandbox --headless --disable-gpu --no-pdf-header-footer --disable-back-forward-cache-for-cache-control-no-store-page --print-to-pdf=./temp.pdf ./index.html
 
 exiftool -overwrite_original -Title="Mil y Un Ghūles - Ambientación de Savage Worlds para poder jugar relatos de horror cósmico en las Mil y Una Noches. Versión 0.2" -Author="@Gwannon" -Subject="Mil y Un Ghūles es una ambientación de Savage Worlds Edición Aventura para poder jugar relatos de horror cósmico en las Mil y Una Noches. Versión 0.2" ./temp.pdf
 
