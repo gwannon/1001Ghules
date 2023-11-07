68aee5299bf4aea752f9961d3d7e85d0d82d8edc
Gwannon <monclus.jorge@gmail.com>
2023-11-07 22:06:59 +0100

    Lod descargas

---
+++ pdf/index.php
@@ -0,0 +1,45 @@
+<?php
+
+date_default_timezone_set("Europe/Madrid");
+
+$filename = __DIR__."/../1001Ghules.pdf";
+
+//Define header information
+header('Content-Description: File Transfer');
+header('Content-Type: application/pdf');
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: 0");
+header('Content-Disposition: attachment; filename="'.basename($filename).'"');
+header('Content-Length: ' . filesize($filename));
+header('Pragma: public');
+
+//Clear system output buffer
+flush();
+
+if(!empty($_SERVER['HTTP_CLIENT_IP'])) {
+ $ip = $_SERVER['HTTP_CLIENT_IP'];
+} 
+else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+ $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} 
+else {
+ $ip = $_SERVER['REMOTE_ADDR'];
+}
+
+
+$json = json_decode(file_get_contents("http://ip-api.com/json/".$ip));
+
+//echo date("Y-m-d H:i:s")."|".$ip."|".$_SERVER['HTTP_USER_AGENT'];
+
+$f=fopen(__DIR__."/log.txt", "a+");
+fwrite($f, date("Y-m-d H:i:s")."|".
+  $ip."|".
+  $_SERVER['HTTP_REFERER']."|".
+  $_SERVER['HTTP_ORIGIN']."|".
+  (isset($json->status) && $json->status == 'success' ? $json->country."|".$json->city : "")."|".
+  $_SERVER['HTTP_USER_AGENT']."\n");
+fclose($f);
+
+//Read the size of the file
+readfile($filename);
+
---
+++ pdf/log.php
@@ -0,0 +1,57 @@
+<?php
+
+$bots = [
+  "facebookexternalhit/1.1",
+  "Semanticbot/1.0",
+  "serpstatbot/2.1",
+  "Twitterbot/1.0",
+  "SemrushBot/7~bl",
+  "bingbot/2.0",
+  "WhatsApp/2",
+  "BW/1.1",
+  "Applebot/0.1",
+  "SeznamBot/4.0",
+  "DataForSeoBot/1.0",
+  "AhrefsBot/7.0",
+  "YandexBot/3.0",
+  "Googlebot-Image/1.0",
+  "Googlebot/2",
+  "IonCrawl",
+  "SemrushBot/7",
+  "help@dataminr.com",
+  "BW/1.1",
+  "Mastodon/4",
+  "Mastodon/3",
+  "craftopi.art",
+  "Bytespider",
+  "Pleroma 2",
+  "Akkoma 3",
+  "TelegramBot",
+  "BLEXBot/1.0",
+  "SerendeputyBot",
+  "SummalyBot",
+  "Friendica",
+  "test-bot",
+  "my-tiny-bot",
+  "thesis-research-bot"
+];
+
+$ips = [];
+$json["total"] = 0;
+$lines = explode("\n", file_get_contents (__DIR__."/log.txt"));
+foreach ($lines as $line) {
+  if($line != '') {
+    $fields =  explode("|", $line);
+    $last_key = array_key_last($fields);
+    if ( !preg_match('('.implode('|',$bots).')', $fields[$last_key])) {
+      $json["descargas"][] = $fields;
+      if(!in_array($fields[1], $ips)) {
+        $ips[] = $fields[1];
+        $json['total'] ++;
+      }
+    }
+  }
+}
+header('Content-Type: application/json; charset=utf-8');
+$json["descargas"] = array_reverse($json["descargas"]);
+echo json_encode($json);
