a75bec9952a830ebe9a3d29217a0baf09c21e9f6
Gwannon <monclus.jorge@gmail.com>
2024-04-06 10:35:36 +0200

    check.php improved

--- pdf/check.php
+++ pdf/check.php
@@ -1,5 +1,5 @@
 <?php
-
+date_default_timezone_set("Europe/Madrid");
 header("Content-Type: text/plain");
 
 $jsons['ArrrCthulhu'] = json_decode(file_get_contents("https://arrrcthulhu.com/pdf/log.php"));
