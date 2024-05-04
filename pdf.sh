523fb4567dbfac5ebfb78fb34275d222483fc2c3
Gwannon <monclus.jorge@gmail.com>
2024-05-04 10:49:51 +0200

    Versión 0.8.1

--- pdf.sh
+++ pdf.sh
@@ -2,7 +2,7 @@
 
 chromium --no-sandbox --enable-features=ConversionMeasurement,AttributionReportingCrossAppWeb --headless --gpu --no-pdf-header-footer --disable-back-forward-cache-for-cache-control-no-store-page --print-to-pdf=./temp.pdf ./index.html
 
-exiftool -overwrite_original -Title="Mil y Un Ghūles - Ambientación de Savage Worlds para poder jugar relatos de horror cósmico en las Mil y Una Noches. Versión 0.8" -Author="@Gwannon" -Subject="Mil y Un Ghūles es una ambientación de Savage Worlds Edición Aventura para poder jugar relatos de horror cósmico en las Mil y Una Noches. Versión 0.8" ./temp.pdf
+exiftool -overwrite_original -Title="Mil y Un Ghūles - Ambientación de Savage Worlds para poder jugar relatos de horror cósmico en las Mil y Una Noches. Versión 0.8.1" -Author="@Gwannon" -Subject="Mil y Un Ghūles es una ambientación de Savage Worlds Edición Aventura para poder jugar relatos de horror cósmico en las Mil y Una Noches. Versión 0.8.1" ./temp.pdf
 
 php pdfIndexGenerator.php > pdf.info
 
