d06c1e71e73e37a5c434aa582b2fd3ac7c4cb3df
gwannon <monclus.jorge@gmail.com>
2025-01-10 23:20:46 +0100

    Update log.php

--- pdf/log.php
+++ pdf/log.php
@@ -302,6 +302,7 @@ $bots['ips'] = [
   "172.253.216.60", //Google
 "35.227.62.178", //Google
 "172.253.192.244", //Google
+  "172.253.15.237", //Google
 
   "128.199.15.211", //Digital Ocean LLC
   "207.154.243.139", //Digital Ocean LLC
