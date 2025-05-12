d4a15823af717f175342da20cb836f95a66e38d5
Gwannon <monclus.jorge@gmail.com>
2025-05-12 23:42:07 +0200

    New bot

--- pdf/log.php
+++ pdf/log.php
@@ -421,6 +421,7 @@ $bots['ips'] = [
   "5.225.20.106", //My IP
   "95.60.100.33", //My IP
   "46.27.198.122", //My IP
+  "47.61.137.64", //My IP
 ];
 
 if(isset($_REQUEST['bots']) && $_REQUEST['bots'] != '') {
